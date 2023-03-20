6cabbac29127ae0afa33193d37a44ed01f2fa122
jonkmcdermott <jonkmcdermott@gmail.com>
2023-03-19 17:59:45 -0700

    start of project

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard/Dashboard';
+import Preferences from './components/Preferences/Preferences';
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+
+          <Route path="/dashboard">
+            <Dashboard/>
+          </Route>
+
+          <Route path="/preferences">
+            <Preferences/>
+          </Route>
+
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
